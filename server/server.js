0e42a0c0a80bf77f8f1e9765e48dc5ca0f889195
kxh <kxh1995@gmail.com>
2019-02-16 01:35:36 +0100

    Added mongoose-delete

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -53,8 +54,27 @@ app.get('/todos/:id', (req, res)=>{
 
     
 })
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+
+app.delete('/todos/:id', (req, res)=>{
+    //get the id
+    //validate id not valid return 404
+    //remove todo by id
+        //success
+            //if no doc, send 404
+            //if doc send doc back
+        //erroor - erro -> 400 with empty body
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+    Todo.findByIdAndRemove(id)
+        .then((todo)=>{
+            if(!todo) res.status(404).send();
+            res.send(todo);
+        }).catch((e)=>res.status(400).send());
+
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
