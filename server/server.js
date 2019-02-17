d6b2baaa8eae1b66283ebf7161f9072e0fa29ade
kxh <kxh1995@gmail.com>
2019-02-17 17:20:27 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -32,45 +33,66 @@ app.get('/todos', (req, res) => {
 });
 
 //GET /todos/123232idgetspassed
-app.get('/todos/:id', (req, res)=>{
+app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectID.isValid(id)) return res.status(404).send();
+    if (!ObjectID.isValid(id)) return res.status(404).send();
     Todo.findById(id)
-        .then((todo)=>{
-            if(!todo) res.status(404).send();
-            res.send({todo})
-        }).catch((e)=>res.status(400).send());
-    
+        .then((todo) => {
+            if (!todo) res.status(404).send();
+            res.send({ todo })
+        }).catch((e) => res.status(400).send());
+
     //validate objectID
     //if not valid stop function execution and respond with 404 send back empy send
 
     //findbyId
-        //success 
-            //if todo -send it back
-                //no todo call succeed send back a 404 withe empy body
-            //error case
-            //400 - is not valid and send empty body back
-    
+    //success 
+    //if todo -send it back
+    //no todo call succeed send back a 404 withe empy body
+    //error case
+    //400 - is not valid and send empty body back
 
-    
+
+
 })
 
-app.delete('/todos/:id', (req, res)=>{
+app.delete('/todos/:id', (req, res) => {
     //get the id
     //validate id not valid return 404
     //remove todo by id
-        //success
-            //if no doc, send 404
-            //if doc send doc back
-        //erroor - erro -> 400 with empty body
+    //success
+    //if no doc, send 404
+    //if doc send doc back
+    //erroor - erro -> 400 with empty body
     let id = req.params.id;
-    if(!ObjectID.isValid(id)) return res.status(404).send();
+    if (!ObjectID.isValid(id)) return res.status(404).send();
     Todo.findByIdAndRemove(id)
-        .then((todo)=>{
-            if(!todo) res.status(404).send();
-            res.send({todo});
-        }).catch((e)=>res.status(400).send());
+        .then((todo) => {
+            if (!todo) res.status(404).send();
+            res.send({ todo });
+        }).catch((e) => res.status(400).send());
 
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(port, () => {
