63a8b4508f7771ab1700c0041b427e59fdbdc87d
kxh <kxh1995@gmail.com>
2019-02-18 15:33:39 +0100

    added POST/users route and hashing.js palygorund

--- server/server.js
+++ server/server.js
@@ -97,6 +97,18 @@ app.patch('/todos/:id', (req, res) => {
     }).catch(e => res.status(400).send());
 });
 
+//POST /users
+app.post('/users', (req, res)=>{
+    var body =_.pick(req.body, ['email', 'password']); 
+    var user = new User(body);
+
+    user.save()
+        .then((user)=>{
+            if(!user) res.status(404).send();
+            res.send(user);
+        }, e=> res.status(400).send(e));
+})
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
